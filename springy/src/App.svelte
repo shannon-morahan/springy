df2ef02a3055d76455bc44ae79f4803cb3bb7ae4
Shannon Morahan <mr.shannon.morahan@gmail.com>
2019-10-18 00:43:31 +0000

    The start

---
+++ springy/src/App.svelte
@@ -0,0 +1,19 @@
+<script>
+    import * as d3 from "d3"
+	export let name;
+
+    const url = window.location;
+    const urlObject = new URL(url);
+    const json = urlObject.searchParams.get('json')
+
+</script>
+
+<style>
+	h1 {
+		color: green;
+	}
+</style>
+
+<div id="my_dataviz"></div>
+
+<h1>Hello There dd {JSON.stringify(json)}!</h1>
