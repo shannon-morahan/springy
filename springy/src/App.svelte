812e033ea48e2615168074ca1825c8d2896d56e9
Shannon Morahan <mr.shannon.morahan@gmail.com>
2019-10-18 02:58:19 +0000

    Added ace edit - still need to move to own component

--- springy/src/App.svelte
+++ springy/src/App.svelte
@@ -1,10 +1,28 @@
 <script>
     import * as d3 from "d3"
+
+import { onMount } from 'svelte';
+
+	onMount(() => {
+		var editor = ace.edit("editor");
+        editor.setTheme("ace/theme/monokai");
+        editor.session.setMode("ace/mode/json");
+
+        const url = window.location;
+        const urlObject = new URL(url);
+        const json = urlObject.searchParams.get('json');
+        editor.getSession().setTabSize(2);
+        editor.getSession().setUseWrapMode(true);
+
+        // const fJsonString = JSON.stringify(json, null, '\t');
+
+        editor.setValue(json);
+        // editor.setValue(json);
+	});
+
 	export let name;
 
-    const url = window.location;
-    const urlObject = new URL(url);
-    const json = urlObject.searchParams.get('json')
+
 
 </script>
 
@@ -12,8 +30,20 @@
 	h1 {
 		color: green;
 	}
+
+    #editor {
+        position: absolute;
+        top: 0;
+        right: 0;
+        bottom: 0;
+        left: 0;
+    }
+
 </style>
 
-<div id="my_dataviz"></div>
+<div id="editor">function foo(items)
 
-<h1>Hello There dd {JSON.stringify(json)}!</h1>
+
+</div>
+
+
